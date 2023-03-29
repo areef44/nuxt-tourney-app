48cec193d9c4b40ad4301fa6f65997308ac72b81
Abdullah  Mochammad Basafi <dola.doit@gmail.com>
2023-03-29 14:10:19 +0700

    membuat fetching data team

--- pages/team/[id].vue
+++ pages/team/[id].vue
@@ -1,88 +1,101 @@
 <template>
-
-<div>
-
+  <div>
     <div class="bg-[#1A1C22] h-full">
-            <div class="flex justify-between">
-                <h1 class="text-white font-bold text-3xl py-4 ml-4">Thursday E-Sports Members</h1>
-                <NuxtLink to="/team">
-                    <p class="btn-del mr-4 my-4">Kembali</p>
-                </NuxtLink>
-            </div>
-    </div>
-    
-    <div class="grid grid-cols-3 gap-1">
-        
-        <div class="p-8 bg-white shadow mt-8 mx-4 rounded-md">
-            <div class="text-center text-gray-800">
-                <h1 class="text-3xl font-semibold my-4">Muhammad Rijal</h1>
-                <p class=""><Icon name="ic:baseline-email" class="mr-1 my-2"/>mhdrijal@rocketmail.com</p>
-                <p class=""><Icon name="ic:baseline-phone-android" class="mr-1 my-2"/>081308130813</p>
-                <p class=""><Icon name="ic:baseline-school" class="mr-1 my-2"/>SMAN 1 DAGO</p>
-                <p class="font-light "><Icon name="ic:baseline-location-city" class="mr-1 my-2"/>Jl. Cisitu Indah VI no 6, Coblong</p>
-            </div>
+      <div class="flex justify-between">
+        <h1 class="text-white font-bold text-3xl py-4 ml-4">
+          {{ teamDetails.name }} E-Sports Members
+        </h1>
+        <div class="text-white font-bold text-xl">
+          <h1>Nama Leader : {{ teamDetails.leader.name }}</h1>
+          <h1>Status Pembayaran : {{ statusPembayaran }}</h1>
+          <button @click="onToggle" class="btn-edit" v-if="statusPembayaran !== 'Lunas' ">Confirm</button>
         </div>
-    
-        <div class="p-8 bg-white shadow mt-8 mx-4 rounded-md">
-            <div class="text-center text-gray-800">
-                <h1 class="text-3xl font-semibold my-4">Muhammad Rijal</h1>
-                <p class=""><Icon name="ic:baseline-email" class="mr-1 my-2"/>mhdrijal@rocketmail.com</p>
-                <p class=""><Icon name="ic:baseline-phone-android" class="mr-1 my-2"/>081308130813</p>
-                <p class=""><Icon name="ic:baseline-school" class="mr-1 my-2"/>SMAN 1 DAGO</p>
-                <p class="font-light "><Icon name="ic:baseline-location-city" class="mr-1 my-2"/>Jl. Cisitu Indah VI no 6, Coblong</p>
-            </div>
-        </div>
-    
-        <div class="p-8 bg-white shadow mt-8 mx-4 rounded-md">
-            <div class="text-center text-gray-800">
-                <h1 class="text-3xl font-semibold my-4">Muhammad Rijal</h1>
-                <p class=""><Icon name="ic:baseline-email" class="mr-1 my-2"/>mhdrijal@rocketmail.com</p>
-                <p class=""><Icon name="ic:baseline-phone-android" class="mr-1 my-2"/>081308130813</p>
-                <p class=""><Icon name="ic:baseline-school" class="mr-1 my-2"/>SMAN 1 DAGO</p>
-                <p class="font-light "><Icon name="ic:baseline-location-city" class="mr-1 my-2"/>Jl. Cisitu Indah VI no 6, Coblong</p>
-            </div>
-        </div>
-    
-        <div class="p-8 bg-white shadow mt-8 mx-4 rounded-md">
-            <div class="text-center text-gray-800">
-                <h1 class="text-3xl font-semibold my-4">Muhammad Rijal</h1>
-                <p class=""><Icon name="ic:baseline-email" class="mr-1 my-2"/>mhdrijal@rocketmail.com</p>
-                <p class=""><Icon name="ic:baseline-phone-android" class="mr-1 my-2"/>081308130813</p>
-                <p class=""><Icon name="ic:baseline-school" class="mr-1 my-2"/>SMAN 1 DAGO</p>
-                <p class="font-light "><Icon name="ic:baseline-location-city" class="mr-1 my-2"/>Jl. Cisitu Indah VI no 6, Coblong</p>
-            </div>
-        </div>
-    
-        <div class="p-8 bg-white shadow mt-8 mx-4 rounded-md">
-            <div class="text-center text-gray-800">
-                <h1 class="text-3xl font-semibold my-4">Muhammad Rijal</h1>
-                <p class=""><Icon name="ic:baseline-email" class="mr-1 my-2"/>mhdrijal@rocketmail.com</p>
-                <p class=""><Icon name="ic:baseline-phone-android" class="mr-1 my-2"/>081308130813</p>
-                <p class=""><Icon name="ic:baseline-school" class="mr-1 my-2"/>SMAN 1 DAGO</p>
-                <p class="font-light "><Icon name="ic:baseline-location-city" class="mr-1 my-2"/>Jl. Cisitu Indah VI no 6, Coblong</p>
-            </div>
-        </div>
-    
-        <div class="p-8 bg-white shadow mt-8 mx-4 rounded-md">
-            <div class="text-center text-gray-800">
-                <h1 class="text-3xl font-semibold my-4">Muhammad Rijal</h1>
-                <p class=""><Icon name="ic:baseline-email" class="mr-1 my-2"/>mhdrijal@rocketmail.com</p>
-                <p class=""><Icon name="ic:baseline-phone-android" class="mr-1 my-2"/>081308130813</p>
-                <p class=""><Icon name="ic:baseline-school" class="mr-1 my-2"/>SMAN 1 DAGO</p>
-                <p class="font-light "><Icon name="ic:baseline-location-city" class="mr-1 my-2"/>Jl. Cisitu Indah VI no 6, Coblong</p>
-            </div>
-        </div>
-    
+        <NuxtLink to="/team">
+          <p class="btn-del mr-4 my-4">Kembali</p>
+        </NuxtLink>
+      </div>
     </div>
 
-</div>
-
+    <div class="grid grid-cols-4 gap-1">
+      <div
+        class="p-8 bg-white shadow mt-8 mx-4 rounded-md"
+        v-for="(member, index) in teamMembers"
+        :key="index"
+      >
+        <div class="text-center text-gray-800">
+          <h1 class="text-3xl font-semibold my-4">{{ member.name }}</h1>
+          <p class="">
+            <Icon name="ic:baseline-email" class="mr-1 my-2" />{{
+              member.email
+            }}
+          </p>
+          <p class="">
+            <Icon name="ic:baseline-phone-android" class="mr-1 my-2" />{{
+              member.phone
+            }}
+          </p>
+          <p class="">
+            <Icon name="ic:baseline-school" class="mr-1 my-2" />{{
+              member.custom_fields
+            }}
+          </p>
+          <p class="font-light">
+            <Icon name="ic:baseline-location-city" class="mr-1 my-2" />{{
+              member.address
+            }}
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-- modal -->
+    <transition name="fade">
+      <div v-if="isModalVisible">
+        <div
+          @click="onToggle"
+          class="absolute bg-black opacity-70 inset-0 z-0"
+        ></div>
+        <div
+          class="bottom-[40%] left-[40%] w-full max-w-lg p-3 absolute mx-auto my-auto rounded-xl shadow-lg bg-white"
+        >
+          <div>
+            <div class="text-center p-3 flex-auto justify-center leading-6">
+              <h2 class="text-2xl font-bold py-4">Konfirmasi Pembayaran</h2>
+              <p class="text-md text-gray-500 px-8">
+                Apakah Anda Ingin Mengkonfirmasi Pembayaran?
+              </p>
+            </div>
+            <div class="p-3 mt-2 text-center space-x-4 md:block">
+              <button class="btn-save" @click="onToggle() ; statusPembayaran='Lunas'">Bayar</button>
+              <button @click="onToggle" class="btn-del">Kembali</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script setup>
-    const { id } = useRoute().params
+const { id } = useRoute().params;
+const headers = {
+  Authorization: "theMyth",
+  "D-App-Authorization":
+    "MjAyMy0wMy0yN1QyMToxNDo1Ny42MTNafGRldi5hbGthZGVtaS5pZHx2T1ZINnNkbXBOV2pSMjcxQ2M3cmR4czAxbHdIemZyMw==",
+};
+const res = await $fetch(
+  `https://services-dev.alkademi.id/v1/v1/team/detail?id=${id}`,
+  { headers }
+);
+const teamDetails = res.data;
+const teamMembers = teamDetails.members;
+const statusPembayaran = ref(teamDetails.status < 1 ? "Belum Lunas" : "Lunas");
+const isOpen = ref(false);
+const isModalVisible = computed(() => isOpen.value);
+console.log(isModalVisible);
+function onToggle() {
+  isOpen.value = !isOpen.value;
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
--- pages/team/index.vue
+++ pages/team/index.vue
@@ -1,128 +1,123 @@
 <template>
-
-    <div class="flex flex-col h-full">
-        <!-- Table -->
-        <div class="w-9/10 mx-6 my-12 bg-white shadow-lg rounded-sm border border-gray-200">
-            <header class="px-5 py-4 border-b border-gray-100">
-                <h2 class="font-semibold text-gray-800">Daftar Team Peserta Turnamen</h2>
-            </header>
-            <div class="p-3">
-                <div class="overflow-x-auto">
-                    <table class="table-auto w-full">
-                        <thead class="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
-                            <tr>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-left">No</div>
-                                </th>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-left">Nama Team</div>
-                                </th>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-left">Email</div>
-                                </th>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-left">Jumlah Anggota</div>
-                                </th>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-center">Alamat</div>
-                                </th>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-center">Action</div>
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody class="text-sm divide-y divide-gray-100">
-                            <tr>
-                                <td class="p-2 whitespace-nowrap">
-                                    <div class="font-medium text-gray-800">1</div>
-                                </td>
-                                <td class="p-2 whitespace-nowrap">
-                                    <div class="flex items-center">
-                                        <div class="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3"><img class="rounded-full" src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-05.jpg" width="40" height="40" alt="Alex Shatov"></div>
-                                        <div class="font-medium text-gray-800">Alex Shatov</div>
-                                    </div>
-                                </td>
-                                <td class="p-2 whitespace-nowrap">
-                                    <div class="text-left">alexshatov@gmail.com</div>
-                                </td>
-                                <td class="p-2 whitespace-nowrap">
-                                    <div class="text-left font-medium text-green-500">$2,890.66</div>
-                                </td>
-                                <td class="p-2 whitespace-nowrap">
-                                    <div class="text-lg text-center">??</div>
-                                </td>
-                                <td class="p-2 whitespace-nowrap">
-                                    <div class="flex justify-center">
-                                        <NuxtLink :to="`/team/{id}`">
-                                            <p class="btn mr-1">Details</p>
-                                        </NuxtLink>
-                                        <div id="app">
-                                            <div class="flex flex-col items-center justify-center min-h-full relative">
-                                                <button @click="onToggle"
-                                                class="btn-edit">
-                                                Confirm
-                                                </button>
-                                            </div>
-                                            <transition name="fade">
-                                                <div v-if="isModalVisible">
-                                                <div @click="onToggle" class="absolute bg-black opacity-70 inset-0 z-0">
-                                                </div>
-                                                <div class="bottom-[40%] left-[40%] w-full max-w-lg p-3 absolute mx-auto my-auto rounded-xl shadow-lg bg-white">
-                                                    <div>
-                                                    <div class="text-center p-3 flex-auto justify-center leading-6">
-                                                        <h2 class="text-2xl font-bold py-4">Konfirmasi Pembayaran</h2>
-                                                        <p class="text-md text-gray-500 px-8">
-                                                            Apakah Anda Ingin Mengkonfirmasi Pembayaran?
-                                                        </p>
-                                                    </div>
-                                                    <div class="p-3 mt-2 text-center space-x-4 md:block">
-                                                        <button class="btn-save">
-                                                        Simpan
-                                                        </button>
-                                                        <button @click="onToggle" class="btn-del">
-                                                        Kembali
-                                                        </button>
-                                                    </div>
-                                                    </div>
-                                                </div>
-                                                </div>
-                                            </transition>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>                                                                 
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+  <div class="flex flex-col h-full">
+    <!-- Table -->
+    <div
+      class="w-9/10 mx-6 my-12 bg-white shadow-lg rounded-sm border border-gray-200"
+    >
+      <header class="px-5 py-4 border-b border-gray-100">
+        <h2 class="font-semibold text-gray-800">
+          Daftar Team Peserta Turnamen
+        </h2>
+      </header>
+      <div class="p-3">
+        <div class="overflow-x-auto">
+          <table class="table-auto w-full">
+            <thead
+              class="text-xs font-semibold uppercase text-gray-400 bg-gray-50"
+            >
+              <tr>
+                <th class="p-2 whitespace-nowrap">
+                  <div class="font-semibold text-left">No</div>
+                </th>
+                <th class="p-2 whitespace-nowrap">
+                  <div class="font-semibold text-left">Nama Team</div>
+                </th>
+                <th class="p-2 whitespace-nowrap">
+                  <div class="font-semibold text-left">Leader</div>
+                </th>
+                <th class="p-2 whitespace-nowrap">
+                  <div class="font-semibold text-center">Jumlah Anggota</div>
+                </th>
+                <th class="p-2 whitespace-nowrap">
+                  <div class="font-semibold text-center">Action</div>
+                </th>
+              </tr>
+            </thead>
+            <tbody class="text-sm divide-y divide-gray-100">
+              <tr v-for="(team, index) in teamPage" :key="index">
+                <td class="p-2 whitespace-nowrap">
+                  <div class="font-medium text-gray-800">
+                    {{ index + 1 + page * 8 }}
+                  </div>
+                </td>
+                <td class="p-2 whitespace-nowrap">
+                  <div class="flex items-center">
+                    <div class="font-medium text-gray-800">{{ team.name }}</div>
+                  </div>
+                </td>
+                <td class="p-2 whitespace-nowrap">
+                  <div class="text-left">{{ team.leader }}</div>
+                </td>
+                <td class="p-2 whitespace-nowrap">
+                  <div class="text-center font-medium text-green-500">
+                    {{ team.totalMember }}
+                  </div>
+                </td>
+                <td class="p-2 whitespace-nowrap">
+                  <div class="flex justify-center">
+                    <NuxtLink :to="`/team/${team.id}`">
+                      <p class="btn mr-1">Details</p>
+                    </NuxtLink>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="flex justify-center gap-5">
+            <button
+              class="bg-lime-300 p-2 rounded-lg"
+              @click="previousTeams"
+              :disabled="page <= 0 ? true : false"
+            >
+              Previous
+            </button>
+            <button
+              class="bg-lime-300 p-2 rounded-lg"
+              @click="nextTeams"
+              :disabled="page >= totalPage - 1 ? true : false"
+            >
+              Next
+            </button>
+          </div>
         </div>
+      </div>
     </div>
-
-    
+  </div>
 </template>
 
-<script>
-    export default {
-    data() {
-      return {
-        isOpen: false
-      };
-    },
-  
-    computed: {
-      isModalVisible() {
-        return this.isOpen;
-      }
-    },
-  
-    methods: {
-      onToggle() {
-        this.isOpen = !this.isOpen;
-      }
-    }
-  };
+<script setup>
+const headers = {
+  Authorization: "theMyth",
+  "D-App-Authorization":
+    "MjAyMy0wMy0yN1QyMToxNDo1Ny42MTNafGRldi5hbGthZGVtaS5pZHx2T1ZINnNkbXBOV2pSMjcxQ2M3cmR4czAxbHdIemZyMw==",
+};
+const res = await $fetch("https://services-dev.alkademi.id/v1/v1/team/list", {
+  headers,
+});
+const Allteams = res.data;
+const teamPage = ref(Allteams.slice(0, 8));
+const totalTeam = ref(Allteams.length);
+const totalPage = Math.ceil(totalTeam.value / 8)
+const page = ref(0);
+console.log(totalPage)
+function previousTeams() {
+    console.log(page.value)
+  if (page.value > 0) {
+    page.value--;
+  }
+  let slide = page.value * 8;
+  teamPage.value = Allteams.slice(slide, slide + 8);
+}
+
+function nextTeams() {
+    
+  if (page.value < totalPage) {
+    page.value++;
+  }
+  let slide = page.value * 8;
+  teamPage.value = Allteams.slice(slide, slide + 8);
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
